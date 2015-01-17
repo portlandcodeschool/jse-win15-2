f3b3f82087b04e8b95df7d25109bbdb161db0f80
Eric Gray <ericxgray@gmail.com>
2015-01-16 16:49:10 -0800

    Defensive cards written sloppily, but passing

--- cards-template1b.js
+++ cards-template1b.js
@@ -1,18 +1,111 @@
 // Error-detecting version
 
-function rank(card) {
+/*
+
+Some trouble I'm having: I can get the contents of checkValidID to work if
+they are all present in the function it is checking, but I cannot
+get the helper function to work. I think it has something to do with the
+the return command, but I am uncertain how to navigate around it at the
+moment.
+
+I also think there's probably a better way to check valid ID's than a bunch
+of ugly ||'s, but this is what I've had time for!
+
+*/
+
+var suitARR = [" Hearts", " Diamonds", " Spades", " Clubs"];
+var rankARR = ["Ace ", "Two ", "Three ", "Four ", "Five ", "Six ", "Seven ", "Eight ", "Nine ", "Ten ", "Jack ", "Queen ", "King "];
+
+function checkValidID(id) {
+			if (id < 0 || id > 51 || id%1 !== 0) {
+		alert("Sorry, your number is out of range. Only whole numbers 0-51, please!)");
+		return NaN;
+			} else if (typeof id === "string" || typeof id === "boolean"
+			 || typeof id === "undefined") {
+		alert("Sorry, only whole numbers 0-51, please!");
+		return NaN;
+	} else {
+		return id;
+	}
+;}
+
+function rank(id) { // --> 1..13
+			if (id < 0 || id > 51 || id%1 !== 0) {
+		alert("Sorry, your number is out of range. Only whole numbers 0-51, please!)");
+		return NaN;
+			} else if (typeof id === "string" || typeof id === "boolean"
+			 || typeof id === "undefined") {
+		alert("Sorry, only whole numbers 0-51, please!");
+		return NaN;
+	};
+	var r = id / 4;
+	return Math.floor(r + 1);
+};
+//}
+
+function suit(id) { // --> 1..4
+			if (id < 0 || id > 51 || id%1 !== 0) {
+		alert("Sorry, your number is out of range. Only whole numbers 0-51, please!)");
+		return NaN;
+			} else if (typeof id === "string" || typeof id === "boolean"
+			 || typeof id === "undefined") {
+		alert("Sorry, only whole numbers 0-51, please!");
+		return NaN;
+	};
+	var s = (id%4)+1;
+	return s
 }
 
-function suit(card) {
+function cardID(rank,suit) { // --> 0..51
+		if (rank <= 0 || rank > 13 || rank%1 !== 0) {
+	alert("Sorry, your number is out of range. Only whole numbers 0-13, please!)");
+	return NaN;
+		} else if (typeof rank === "string" || typeof rank === "boolean"
+		|| typeof rank === "undefined") {
+	alert("Sorry, only whole numbers 0-13, please!");
+	return NaN;
+};
+		if (suit <= 0 || suit > 4 || suit%1 !== 0) {
+	alert("Sorry, your number is out of range. Only whole numbers 0-4, please!)");
+	return NaN;
+		} else if (typeof suit === "string" || typeof suit === "boolean"
+		|| typeof suit === "undefined") {
+	alert("Sorry, only whole numbers 0-4, please!");
+	return NaN;
+};
+	var cid = ((rank * 4) - 5)  + suit;
+	return cid;
 }
 
-function cardID(rank,suit) {
+function color(id) { // -->"red","black"
+			if (id < 0 || id > 51 || id%1 !== 0) {
+		alert("Sorry, your number is out of range. Only whole numbers 0-51, please!)");
+		return NaN;
+			} else if (typeof id === "string" || typeof id === "boolean"
+			 || typeof id === "undefined") {
+		alert("Sorry, only whole numbers 0-51, please!");
+		return NaN;
+	};
+		if (suit(id) == 1 || suit(id) == 2){
+		return "red";
+	} else if (suit(id) == 3 ||  suit(id) == 4){
+		return "black";
+	};
 }
 
-function color(card) {
-}
-
-function name(card) {
+function name(id) { // --> string
+			if (id < 0 || id > 51 || id%1 !== 0) {
+		alert("Sorry, your number is out of range. Only whole numbers 0-51, please!)");
+		return NaN;
+			} else if (typeof id === "string" || typeof id === "boolean"
+			 || typeof id === "undefined") {
+		alert("Sorry, only whole numbers 0-51, please!");
+		return NaN;
+	};
+	var rid = rank(id) - 1;
+	var sid = suit(id) - 1;
+	var name = rankARR[rid] + "of" + suitARR[sid];
+	return name;
 }
 
 
@@ -65,4 +158,3 @@ assert(Number.isNaN(name(false)),   "Test 43 failed");
 assert(Number.isNaN(name(-1)),      "Test 44 failed");
 assert(Number.isNaN(name(52)),      "Test 45 failed");
 assert(Number.isNaN(name(NaN)),     "Test 46 failed");
-
