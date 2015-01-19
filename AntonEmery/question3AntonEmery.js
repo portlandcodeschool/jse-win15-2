afa3f4cffb8bc262ed86dcf1f94215273a949804
AntonEmery <antonemery@gmail.com>
2015-01-19 13:20:32 -0800

    problem 3

--- AntonEmery/question3AntonEmery.js
+++ AntonEmery/question3AntonEmery.js
@@ -3,11 +3,17 @@
 var days = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 
 			'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth'];
 
-var gifts = [];			
+var gifts = ['A Partridge in a Pear Tree', '2 Turtle Doves', '3 French Hens', '4 Calling Birds',
+			'5 Golden Rings', '6 Geese a Laying', '7 Swans a Swimming', '8 Maids a Milking',
+			'9 Ladies Dancing', '10 Lords a Leaping', '11 Pipers Piping', '12 Drummers Drumming'];			
 
 
 //initial loop that prints out 'On the X day of Christmas'
 
-for(i = 1; i < 13; i++) {
-	console.log('On the ' + days[i - 1] + ' day of Christmas my true love gave to me');
-}
+for(var i = 0; i < 12; i++) {
+	console.log('On the ' + days[i] + ' day of Christmas my true love gave to me');
+	console.log(gifts[i]);
+	//if gifts[i] > 0
+		//counter = gifts[i]; counter >= 0; counter--
+		//print gifts[counter]
+	};
