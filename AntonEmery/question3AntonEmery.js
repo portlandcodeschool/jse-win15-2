a3aa25af98a18a4b084d75c659a9109ec6f6d7c6
AntonEmery <antonemery@gmail.com>
2015-01-19 12:42:01 -0800

    prob 5 done, starting on 3

---
+++ AntonEmery/question3AntonEmery.js
@@ -0,0 +1,13 @@
+
+//Days array
+var days = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 
+			'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth'];
+
+var gifts = [];			
+
+
+//initial loop that prints out 'On the X day of Christmas'
+
+for(i = 1; i < 13; i++) {
+	console.log('On the ' + days[i - 1] + ' day of Christmas my true love gave to me');
+}
