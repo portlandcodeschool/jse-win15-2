ff02fd1041cf71422e1c978781e08942af66f90f
AntonEmery <antonemery@gmail.com>
2015-01-19 16:47:14 -0800

    question 3

--- AntonEmery/question3AntonEmery.js
+++ AntonEmery/question3AntonEmery.js
@@ -3,9 +3,9 @@
 var days = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 
 			'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth'];
 
-var gifts = ['A Partridge in a Pear Tree', '2 Turtle Doves', '3 French Hens', '4 Calling Birds',
-			'5 Golden Rings', '6 Geese a Laying', '7 Swans a Swimming', '8 Maids a Milking',
-			'9 Ladies Dancing', '10 Lords a Leaping', '11 Pipers Piping', '12 Drummers Drumming'];			
+var gifts = ['A Partridge in a Pear Tree', 'Two Turtle Doves', 'Three French Hens', 'Four Calling Birds',
+			'Five Golden Rings', 'Six Geese a Laying', 'Seven Swans a Swimming', 'Eight Maids a Milking',
+			'Nine Ladies Dancing', 'Ten Lords a Leaping', 'Eleven Pipers Piping', 'Twelve Drummers Drumming'];			
 
 
 //initial loop that prints out 'On the X day of Christmas'
@@ -14,10 +14,16 @@ for(var i = 0; i < 12; i++) {
 	console.log('On the ' + days[i] + ' day of Christmas my true love gave to me');
 	console.log(gifts[i]);
 		if (gifts.indexOf(gifts[i]) > 0){
-			for(counter = gifts.indexOf(gifts[i]); counter >= 0; counter--) {
+			for(counter = gifts.indexOf(gifts[i]); counter > 0; counter--) {
 					console.log(gifts[counter-1]);
 			}
 		}
 };
 
 //I know this is not exactly the format you want. as far as i could get right now
+
+function printDays () {
+	for(var i = 0; i < 12; i++) {
+	var x = 'On the ' + days[i] + ' day of Christmas my true love gave to me';
+	};
+		}
